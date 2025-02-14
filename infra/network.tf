7b6bffa0d0450902b84740bbf1fecb3d55351398
Alan Luiz Viana <alanlviana@hotmail.com>
2025-02-14 13:09:01 -0300

    build: network.tf

--- infra/network.tf
+++ infra/network.tf
@@ -59,7 +59,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id            = aws_vpc.vpc_ecs.id
   service_name      = "com.amazonaws.sa-east-1.ecr.api"
   vpc_endpoint_type = "Interface"
-  subnet_ids        = [aws_subnet.private_subnet_a, aws_subnet.private_subnet_b]  # Replace with your subnet IDs
+  subnet_ids        = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
 
   tags = {
     Name = "ECR API Endpoint"
