d71b3f03b2ceec155832b03284f2f1ace460951d
Alan Luiz Viana <alanlviana@hotmail.com>
2025-02-14 11:40:44 -0300

    fix: load balancer resource name

--- infra/main.tf
+++ infra/main.tf
@@ -87,7 +87,7 @@ resource "aws_lb_target_group" "app_tg" {
 }
 
 resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.example.arn
+  load_balancer_arn = aws_lb.app_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
