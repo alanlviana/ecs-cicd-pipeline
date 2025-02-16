3d14b447c625584c57badad6e57b762edc5b0e99
Alan Luiz Viana <alanlviana@hotmail.com>
2025-02-16 11:22:15 -0300

    build: updating pipeline

--- infra/network.tf
+++ infra/network.tf
@@ -1,7 +1,10 @@
+
+# Get Availability Zones
 data "aws_availability_zones" "available_zones" {
   state = "available"
 }
 
+// Create VPC
 resource "aws_vpc" "vpc_ecs" {
   cidr_block = var.vpc_cidr
   
@@ -10,6 +13,7 @@ resource "aws_vpc" "vpc_ecs" {
   })
 }
 
+// Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc_ecs.id
 
@@ -18,8 +22,44 @@ resource "aws_internet_gateway" "igw" {
   })
 }
 
+// Create Public Subnets and Route table using the Internet Gateway
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.vpc_ecs.id
+
+  count             = 2
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    Name = "${var.app_name}-public-subnet-${count.index + 1}"
+  })
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_ecs.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.app_name}-public-route-table"
+  })
+
+}
+
+resource "aws_route_table_association" "public_rt_asso" {
+  count          = 2
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
 
 
+
+// Create Private Subnets and Route table using the NAT Gateway through the Public Subnets
 resource "aws_subnet" "private_subnets" {
   vpc_id = aws_vpc.vpc_ecs.id
 
@@ -65,36 +105,3 @@ resource "aws_route_table" "private_route_table" {
   })
 }
 
-resource "aws_subnet" "public_subnets" {
-  vpc_id = aws_vpc.vpc_ecs.id
-
-  count             = 2
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-
-  map_public_ip_on_launch = true
-
-  tags = merge(local.common_tags, {
-    Name = "${var.app_name}-public-subnet-${count.index + 1}"
-  })
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc_ecs.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = merge(local.common_tags, {
-    Name = "${var.app_name}-public-route-table"
-  })
-
-}
-
-resource "aws_route_table_association" "public_rt_asso" {
-  count          = 2
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-  route_table_id = aws_route_table.public_route_table.id
-}
--- infra/security.tf
+++ infra/security.tf
@@ -1,3 +1,4 @@
+// Create Execution Role for ECS Task
 resource "aws_iam_role" "ecs_task_execution_role" {
     name = "${var.app_name}-execution-role"
 
@@ -20,6 +21,8 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+// Create Security Groups for Load Balancer and Application
+
 resource "aws_security_group" "app_sg" {
     name        = "${var.app_name}-sg"
     description = "Allow HTTP inbound traffic"
@@ -29,14 +32,14 @@ resource "aws_security_group" "app_sg" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        security_groups = [ aws_security_group.lb_sg.id ]
+        security_groups = [ aws_security_group.lb_sg.id ] // Allow traffic from Load Balancer
     }
 
     ingress {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        security_groups = [ aws_security_group.lb_sg.id ]
+        security_groups = [ aws_security_group.lb_sg.id ] // Allow traffic from Load Balancer
     }
 
     egress {
@@ -56,7 +59,7 @@ resource "aws_security_group" "lb_sg" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"] // Allow traffic from anywhere
     }
 
     egress {
